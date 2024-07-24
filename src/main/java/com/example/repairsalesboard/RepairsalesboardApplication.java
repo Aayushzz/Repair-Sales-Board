7b765955100fea4ca3f217bdf3c58c0b325eee2c
Aayushzz <aayushnrla@gmail.com>
2024-07-24 18:05:31 -0400

    Project Initialization

---
+++ src/main/java/com/example/repairsalesboard/RepairsalesboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.repairsalesboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairsalesboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairsalesboardApplication.class, args);
+	}
+
+}
